2fb9f353bc3d20e8584c501aca2cdf0df87c05a2
Laura Montenegro <larislakis@hotmail.com>
2023-01-10 15:01:03 -0300

    Add project files.

---
+++ Models/Reserva.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Suite selecionada não suporta a quantidade de hóspedes desta reserva.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor * 0.1M);
+            }
+            return valor;
+        }
+    }
+}
